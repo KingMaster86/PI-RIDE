9101765bce482b3c6aae7b84b31cb2278696520d
KingMaster86 <kingmastermassage2@gmail.com>
2025-01-01 01:14:27 +0700

    Create index.ts

---
+++ app/src/index.ts
@@ -0,0 +1,21 @@
+index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Ubah dari `serviceWorker.unregister()` menjadi `serviceWorker.register()`
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Pendaftaran Service Worker
+serviceWorkerRegistration.register();
+
+reportWebVitals();
