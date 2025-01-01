620f733376115372e496a262678c79d70cfce85b
KingMaster86 <kingmastermassage2@gmail.com>
2025-01-01 07:33:08 +0700

    Create home.js

---
+++ app/src/App/home.js
@@ -0,0 +1,24 @@
+// app/home.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the Home Page!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
